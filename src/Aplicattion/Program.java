2c236bfb9232c190a06e016595c1ef79ed0feae1
Joao Victor Zanoni <jvzanoni111@gmail.com>
2020-04-14 20:53:37 -0300

    Print Possible

--- src/Aplicattion/Program.java
+++ src/Aplicattion/Program.java
@@ -19,11 +19,15 @@ public class Program {
 		
 		while (true) {
 			try {
-				UI.clearScreen();
 				
+				UI.clearScreen();
 				UI.printBoard(chessMatch.getPieces());
 				System.out.print("\nSource: ");
 				ChessPosition source = UI.readChessPosition(sc);
+				
+				boolean[][] possibleMoves = chessMatch.possibleMoves(source);
+				UI.clearScreen();
+				UI.printBoard(chessMatch.getPieces(), possibleMoves);
 				
 				System.out.println();
 				System.out.print("Target: ");
